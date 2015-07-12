da24b634ace1bc32d635b12c320692ebe64256f2
natsutan <YRW03704@nifty.com>
2015-07-12 19:39:25 +0900

    コンストラクタ作成中

--- sample/sample.py
+++ sample/sample.py
@@ -3,4 +3,6 @@ __author__ = 'mnatsutani'
 import myhdl
 from src.myhdl_fixed import Signal_fixed
 
-val = Signal_fixed(w=8,i=4, q='SC_RND', o='SC_SAT')
+val = Signal_fixed(w=8,i=4, q='SC_RND', o='SC_SAT', val = -2.5)
+print(len(val))
+print("val = %d" % val)
